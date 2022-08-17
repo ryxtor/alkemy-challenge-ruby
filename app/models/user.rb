0b44125e98c32072c4a5eefe9dcbed0173a6585f
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-08-17 16:29:27 -0300

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_create :generate_token
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+  private
+
+  def add_token
+    self.token = JwtHelper::JsonWebToken.encode(email)
+  end
+end
