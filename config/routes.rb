5fe814154cbeb12cc22f574f5364474d99d3af46
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-08-17 19:46:40 -0300

    Add character routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Users routes
-  post 'v1/user', to: 'users#authenticate', as: 'authentication_user'
-  post 'v1/user/add', to: 'users#add', as: 'add_user_path'
+  post 'v1/user', to: 'v1/users#authenticate', as: 'authentication_user'
+  post 'v1/user/add', to: 'v1/users#add', as: 'add_user_path'
+
+  namespace :v1 do
+    resources :characters, only: %i[index show create update destroy]
+  end
 end
