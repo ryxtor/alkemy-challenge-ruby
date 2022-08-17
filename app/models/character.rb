4d7d8bbb3630534bd6b7a8594ec2ff307099a538
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-08-17 19:45:36 -0300

    Add character model

---
+++ app/models/character.rb
@@ -0,0 +1,9 @@
+class Character < ApplicationRecord
+  has_one_attached :image
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 150 }
+  validates :weight, presence: true, numericality: { greater_than: 0, less_than: 150 }
+  validates :history, presence: true, length: { maximum: 2000 }
+  validates :image, presence: true
+end
