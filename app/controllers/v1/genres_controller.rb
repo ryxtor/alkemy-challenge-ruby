820555130751aeea8e865f7c25c80d67edef514a
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-08-18 00:26:36 -0300

    Add genres controller

---
+++ app/controllers/v1/genres_controller.rb
@@ -0,0 +1,88 @@
+class V1::GenresController < ApplicationController
+  def user_validation
+    user, error, status = UsersHelper::Validator.valid_user_token?(request.headers['Authorization'])
+    unless user
+      render json: { 'error:': error }, status: status
+      return
+    end
+    user
+  end
+
+  def create
+    return unless user_validation
+
+    genre = Genre.new(genre_params)
+    if genre.save
+      render json: genre.to_json(only: %I[id name]), status: :ok
+    else
+      render json: { 'error:': genre.errors.first.message }, status: :bad_request
+    end
+  rescue StandardError => e
+    render json: { 'error:': e }, status: :bad_request
+  end
+
+  def index
+    return unless user_validation
+
+    genres = if params[:name]
+               Genre.where('name ~* ?', params[:name])
+             else
+               Genre.all
+             end
+
+    render json: genre_index(genres).to_json(only: %I[id name]), status: :ok
+  rescue StandardError => e
+    render json: { 'error:': e }, status: :bad_request
+  end
+
+  def show
+    return unless user_validation
+
+    genre = Genre.find(params[:id])
+    render json: genre_show(genre).to_json(only: %I[id name]), status: :ok
+  rescue StandardError => e
+    render json: { 'error:': e }, status: :bad_request
+  end
+
+  def update
+    return unless user_validation
+
+    genre = Genre.find(params[:id])
+    if genre.update(genre_params)
+      render json: genre.to_json(only: %I[id name]), status: :ok
+    else
+      render json: { 'error:': genre.errors.first.message }, status: :bad_request
+    end
+  rescue StandardError => e
+    render json: { 'error:': e }, status: :bad_request
+  end
+
+  def destroy
+    return unless user_validation
+
+    genre = Genre.find(params[:id])
+    if genre.destroy
+      render json: { message: 'Genre deleted' }, status: :ok
+    else
+      render json: { 'error:': genre.errors.first.message }, status: :bad_request
+    end
+  rescue StandardError => e
+    render json: { 'error:': e }, status: :bad_request
+  end
+
+  private
+
+  def genre_params
+    params.permit(:name, :image)
+  end
+
+  def genre_index(genres)
+    genres.map do |genre|
+      { id: genre.id, name: genre.name, image: genre.image.attachment.url }
+    end
+  end
+
+  def genre_show(genre)
+    { id: genre.id, name: genre.name, image: genre.image.attachment.url, movies: genre.movies }
+  end
+end
