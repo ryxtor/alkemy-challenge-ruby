4706246003a2f6bd2a49ef24d1123e3886bbad2c
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-08-18 00:25:06 -0300

    Add movie model

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ApplicationRecord
+  has_one_attached :image
+  has_and_belongs_to_many :characters, dependent: :destroy
+  has_and_belongs_to_many :genres, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :release_date, presence: true
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
+  validates :image, presence: true
+end
